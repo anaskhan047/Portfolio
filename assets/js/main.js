16b162ac49fedcea1daf08909d08614094cc684d
Anas Khan <anaskhany47@gmail.com>
2025-07-07 13:04:44 +0530

    First commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -280,3 +280,29 @@ if (form) {
     });
   });
 }
+  function calculateAge(dob) {
+    const birthDate = new Date(dob);
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+
+    return age;
+  }
+
+  const dobElement = document.querySelector(".dob");
+
+  if (dobElement) {
+    const dobText = dobElement.textContent.trim();
+    const dobParsed = new Date(dobText);
+    const age = calculateAge(dobParsed);
+
+    const ageSpan = document.getElementById("my-age");
+    if (ageSpan) {
+      ageSpan.textContent = age;
+    }
+  }
